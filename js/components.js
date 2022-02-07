4443cf2862fb62173e5aea05613c6e6c95cf06a1
MiguelBarreraDev <barreram987@gmail.com>
2022-02-07 02:08:49 -0500

    Adding script_name.js files
    Contain files:
    	cards.json -> Simulate data base, contain the questions and
    	responses
    	components.js -> Contain the components(html view)
    	main.js -> Contain the logical of the program

---
+++ js/components.js
@@ -0,0 +1,94 @@
+import { arrCards } from "/js/main.js";
+export  { 
+	createElement, insertElement , showViewCard , renderBtn, renderCountBox
+};
+
+function createElement(elementData)
+{
+	let {
+		id,
+		question,
+		response
+	} = elementData;
+
+	let component = document.createElement("div");
+	let elmBoxQuestion = document.createElement("div");
+	let elmBoxResponse = document.createElement("div");
+	let elmQuestion = document.createElement("div");
+
+	component.classList.add("box", `box-${id}`);
+	component.setAttribute("id", id);
+
+	/* Part - create question box element */
+	elmBoxQuestion.classList.add("box-question");
+	elmBoxQuestion.setAttribute("id", `box-question_${id}`);
+	elmQuestion.classList.add("que", `que-${id}`);
+	elmQuestion.setAttribute("id", `que-${id}`);
+	elmQuestion.innerText = question;
+	elmBoxQuestion.appendChild(elmQuestion);
+
+	/* Part - create response box element */
+	let arrKey = Object.keys(response);
+	elmBoxResponse.classList.add("box-response");
+	elmBoxResponse.setAttribute("id", `box-response_${id}`);
+	for (let i in arrKey) {
+		let elmResponse = document.createElement("a");
+
+		elmResponse.classList.add("res", `res-${i}`);
+		elmResponse.setAttribute("id", `res-${i}`);
+		elmResponse.setAttribute("tabindex", parseInt(i) + 1) /*Added to use focus pseudo-class*/
+		elmResponse.innerText = response[arrKey[i]];
+		elmBoxResponse.appendChild(elmResponse);
+	}
+
+	component.appendChild(elmBoxQuestion);
+	component.appendChild(elmBoxResponse);
+
+	return component;
+}
+
+function insertElement(element) {
+	const ctnBox = document.getElementById("ctn-box")
+
+	try {
+		ctnBox.removeChild(ctnBox.querySelector(".box"));
+	}
+	catch (e) {
+		console.error("Error: The first card was loaded");
+	}
+	finally {
+		ctnBox.appendChild(element);
+		let currentBox = document.getElementById(`${element.getAttribute("id")}`);
+		let responseBox = currentBox.querySelector(".box-response");
+		responseBox.classList.add("show");
+	}
+}
+
+function showViewCard(id=0) {
+	let data = arrCards[id]; 
+	let element = createElement(data);
+	
+	insertElement(element);
+}
+
+function renderBtn() {
+	const ctnHandler = document.querySelector(".handler");
+	const btnNext = ctnHandler.querySelector(".btn-next");
+	const btnPrev = ctnHandler.querySelector(".btn-prev");
+	const btnFinish = btnNext.cloneNode();
+	
+	btnFinish.innerText = "Finish";
+	btnFinish.classList.add("btn-finish");
+	btnFinish.classList.remove("btn-next");
+
+	ctnHandler.removeChild(btnNext);
+	ctnHandler.removeChild(btnPrev);
+	ctnHandler.appendChild(btnFinish);
+}
+
+function renderCountBox(specifie, total=arrCards.length - 1)
+{
+	const countBox = document.querySelector(".count-box");
+
+	countBox.innerText = `${specifie} / ${total}`;
+}
---
+++ js/main.js
@@ -0,0 +1,56 @@
+import {
+	createElement, insertElement , showViewCard, renderBtn, renderCountBox
+} from "/js/components.js";
+export { arrCards };
+
+const btnPrev = document.getElementById("btn-prev"),
+	btnNext = document.getElementById("btn-next");
+let arrCards = await fetch("/js/cards.json")
+	.then(res => res.json());
+
+/*****************************************/
+/* Anonime function to show the first view
+/*****************************************/
+((id=0) => {
+	let data = arrCards[id]; 
+	let element = createElement(data);
+
+	renderCountBox(0, arrCards.length - 1);
+	insertElement(element);
+})()
+
+/*****************************************/
+/* Implementing controllers (buttons)
+/*****************************************/
+let itr = 0;
+
+btnNext.addEventListener("click", () => {
+	let nextBoxId = 0;
+	
+	if (itr === arrCards.length - 1) {
+		return null;
+	}
+	itr++;
+	if (itr == arrCards.length - 1) {
+		renderBtn();
+	}
+	nextBoxId = arrCards[itr].id;
+	renderCountBox(itr);
+	showViewCard(nextBoxId);
+})
+
+btnPrev.addEventListener("click", () => {
+	let prevBoxId = 0;
+	
+	if (itr === 0) {
+		return null;
+	}
+	itr--
+	prevBoxId = arrCards[itr].id;
+	renderCountBox(itr);
+	showViewCard(prevBoxId);
+})
+
+/*========================*/
+/* Logica
+/*========================*/
